dc985abedda6600f6a58827cc792bc0316d00d20
rowan <rowan@rowan.website>
2018-12-03 16:56:34 -0500

    Reconfigured some stuff, includes an incorrect partial solution to 2-1

--- 2/1.js
+++ 2/1.js
@@ -4,5 +4,17 @@
 */
 
 const fs = require('fs')
-const input = fs.readFileSync('input').toString().split('\n')
-console.log(input)
+const inputs = fs.readFileSync('input').toString().split('\n')
+let twos = 0
+let threes = 0
+
+inputs.forEach(input => {
+  [...input].forEach(val => {
+    let dupes = [...input].filter(val => val===val)
+    if (dupes.length === 2) {
+      twos++
+    } else if (dupes.length === 3) {
+      threes++
+    }
+  })
+})
--- 2/2.js
+++ 2/2.js
@@ -2,3 +2,7 @@
 * Advent of Code 2018
 * day 2, problem 2
 */
+
+const fs = require('fs')
+const inputs = fs.readFileSync('input').toString().split('\n')
+
