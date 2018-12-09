86d763709a219d7419eb9cac7fbcf9964fdfd002
rowan <rowan@rowan.website>
2018-12-08 22:10:26 -0500

    Parse incoming data

--- 3/1.js
+++ 3/1.js
@@ -5,3 +5,22 @@
 
 const fs = require('fs')
 const inputs = fs.readFileSync('input').toString().split('\n')
+                  .map(parseClaim)
+
+function parseClaim (claim) {
+  let id = Number(claim.split(/[#@]/)[1])
+  let coord = [Number(claim.split(/[@,:]/)[1]), Number(claim.split(/[@,:]/)[2])]
+  let size = [Number(claim.split(/[:x]/)[1]), Number(claim.split(/[:x]/)[2])]
+  return [id, coord, size]
+}
+
+let duplicates = 0
+let fabricAreas = []
+
+/*
+probably do this with an object of arrays? my off the cuff idea here is to
+give each array a value of 1 when it's first found, and if it already had a 1 in
+it then we'll increment a counter. If it had a 2 or above, don't increment the
+counter.
+*/
+
