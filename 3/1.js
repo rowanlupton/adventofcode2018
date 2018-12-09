721199e11a00e580ddf585ceeef695ca1f756396
rowan <rowan@rowan.website>
2018-12-08 22:49:33 -0500

    Incorrect first attempt

--- 3/1.js
+++ 3/1.js
@@ -11,14 +11,31 @@ function parseClaim (claim) {
   claim = claim.split(/[#@,:x]/)
 
   let id = Number(claim[1])
-  let coord = [Number(claim[2]), Number(claim[3])]
-  let size = [Number(claim[4]), Number(claim[5])]
+  let coordinates = {x: Number(claim[2]), y: Number(claim[3])}
+  let size = {x: Number(claim[4]), y: Number(claim[5])}
 
-  return [id, coord, size]
+  return {id: id, coordinates: coordinates, size: size}
 }
 
 let duplicates = 0
 let fabricAreas = []
+
+inputs.forEach(input => {
+  for (let  x = input.coordinates.x;
+            x < input.coordinates.x + input.size.x;
+            x ++) {
+    if (!fabricAreas[x]) fabricAreas[x] = []
+    for (let  y = input.coordinates.y;
+              y < input.coordinates.y + input.size.x;
+              y++) {
+      if (!fabricAreas[x][y]) fabricAreas[x][y] = 0
+      fabricAreas[x][y]++
+      if (fabricAreas[x][y] === 2) duplicates++
+    }
+  }
+})
+
+console.log(duplicates)
 
 /*
 probably do this with an object of arrays? my off the cuff idea here is to
@@ -27,3 +44,7 @@ it then we'll increment a counter. If it had a 2 or above, don't increment the
 counter.
 */
 
+/*
+* WRONG ANSWERS
+* 123369 (too high)
+*/
