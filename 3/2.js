73a83188b9376b83a9ad70019fce9e62d410a17f
rowan <rowan@rowan.website>
2018-12-08 23:00:16 -0500

    Shell outline setup

--- 3/2.js
+++ 3/2.js
@@ -5,3 +5,31 @@
 
 const fs = require('fs')
 const inputs = fs.readFileSync('input').toString().split('\n')
+                  .map(parseClaim)
+
+function parseClaim (claim) {
+  claim = claim.split(/[#@,:x]/)
+
+  let id = Number(claim[1])
+  let coordinates = {x: Number(claim[2]), y: Number(claim[3])}
+  let size = {x: Number(claim[4]), y: Number(claim[5])}
+
+  return {id: id, coordinates: coordinates, size: size}
+}
+
+let fabricAreas = []
+
+inputs.forEach(input => {
+  for (let  x = input.coordinates.x;
+            x < input.coordinates.x + input.size.x;
+            x++) {
+    if (!fabricAreas[x]) fabricAreas[x] = []
+    for (let  y = input.coordinates.y;
+              y < input.coordinates.y + input.size.y;
+              y++) {
+      if (!fabricAreas[x][y]) fabricAreas[x][y] = 0
+      fabricAreas[x][y]++
+      if (fabricAreas[x][y] > 1) duplicates++
+    }
+  }
+})
